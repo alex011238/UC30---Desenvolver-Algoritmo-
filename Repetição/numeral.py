cbdad1b7a78ad14d4130eca409e13fe9f1bf79d0
alex011238 <luangrrodrigues@gmail.com>
2026-03-03 14:43:45 -0300

    Add files via upload

---
+++ Repetição/numeral.py
@@ -0,0 +1,29 @@
+
+ senhaCorreta = "12345"
+
+senha = input("Digite seu nome: ", (nome) {
+  print("Digite sua senha: ",(senha) {
+
+    if (senha == senhaCorreta) {
+      ("Olá, {nome}. Seja bem-vindo ao nosso banco!");
+    } else {
+      print("Senha incorreta!");
+    }
+
+while
+    senha = input("Digite sua senha: ")
+
+    if
+        print("Acesso permitido!")
+
+    else:
+
+        if
+            print("Senha incorreta! Você ainda tem 1 tentativa, senhor.")
+        elif
+            print("Sua senha foi bloqueada! Por favor, dirija-se a um de nossos caixas")
+        else:
+            print("Senha incorreta!")
+
+  });
+})
