316343994f85fc7470ee48b0ab37367716a9482f
alex011238 <luangrrodrigues@gmail.com>
2026-04-07 14:05:00 -0300

    Add files via upload

---
+++ OBI/obi.py
@@ -0,0 +1,23 @@
+import ong
+
+def resolver(valor):
+
+     try:
+       
+       if not linha:
+          return
+        P, R,B = map(int, linha.spirt())
+
+
+     if  p == 0:
+     print("C")
+     elif p == 1, B == 2 and r == 0:
+     print ("B")
+     else:
+     print ("A")
+
+    except ValueError
+        pass
+      
+
+    resolver()
