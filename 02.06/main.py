b8dd4d9257ca9c6b2b1f876fb7aeda522c30dd9d
alex011238 <luangrrodrigues@gmail.com>
2026-06-02 14:27:07 -0300

    Add files via upload

---
+++ 02.06/main.py
@@ -0,0 +1,20 @@
+@app.route('/')
+def formulario():
+    return render_template('index.html')
+
+
+@app.route('/validacao', methods=['POST'])
+def cadastro():
+
+    nome = request.form.get('nome', '').strip().title()
+    email = request.form.get('emial', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().title()
+
+    return f"""
+    Nome: {nome}<br>
+    Email: {email}<br>
+    Cidade: {cidade}<br>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=true)
